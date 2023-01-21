2cf8319fde64c263118a139fc0bcea4c968eb2c7
Wojciech Glowa <wojciechrajdom@gmail.com>
2023-01-21 21:56:29 +0100

    Zadanie domowe Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World Wojtek G!");
+﻿var name = "Wojtek";
+var gender = "Meżczyzna";
+var age = 37;
+
+if (age > 40 && gender == "Mężczyzna")
+{
+    Console.WriteLine("Mężczyzna powyżej 40 lat");
+}
+else if (name == "Wojtek" && age == 37)
+{
+    Console.WriteLine("Jestem Wojtek i mam 37 lat");
+}
+else if (age == 17)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
