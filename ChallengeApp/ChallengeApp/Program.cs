5166425dc223390eed42fbeb109895d2b8e1ea2d
wojciechrajdom <wojciechrajdom@gmail.com>
2024-05-16 17:22:29 +0200

    Zadanie Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Wojtek";
+var gender = "Mężczyzna";
+var age = 40;
+
+if (age > 45 && gender == "Mężczyzna")
+{
+    Console.WriteLine("Mężczyzna powyżej 45 lat");
+}
+else if (name =="Wojtek" && age == 40)
+{
+    Console.WriteLine("Mam na imię Wojtek i mam 40 lat");
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
