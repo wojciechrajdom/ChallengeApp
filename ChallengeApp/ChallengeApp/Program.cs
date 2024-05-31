6b627b0d5918044969cfdd2c7227fcef1c9b6e95
wojciechrajdom <wojciechrajdom@gmail.com>
2024-05-31 15:59:01 +0200

    Zadanie dzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,30 +1,56 @@
-﻿int number = 5129;
+﻿using ChallengeApp;
 
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+Employee employee1 = new Employee("Wojtek", "Glowa", "40");
+Employee employee2 = new Employee("Kubuś", "Puchatek", "10");
+Employee employee3 = new Employee("John", "Wick", "50");
 
-List<char> numbers = new List<char>();
-numbers.Add('0');
-numbers.Add('1');
-numbers.Add('2');
-numbers.Add('3');
-numbers.Add('4');
-numbers.Add('5');
-numbers.Add('6');
-numbers.Add('7');
-numbers.Add('8');
-numbers.Add('9');
+employee1.AddScore(1);
+employee1.AddScore(3);
+employee1.AddScore(5);
+employee1.AddScore(7);
+employee1.AddScore(9);
 
-foreach (var item in numbers)
+employee2.AddScore(9);
+employee2.AddScore(7);
+employee2.AddScore(5);
+employee2.AddScore(3);
+employee2.AddScore(1);
+
+employee3.AddScore(8);
+employee3.AddScore(6);
+employee3.AddScore(3);
+employee3.AddScore(4);
+employee3.AddScore(7);
+
+List<Employee> employees = new List<Employee>()
 {
-    int count = 0;
-    foreach (var letter in letters)
+    employee1 , employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+
+foreach (var employee in employees)
+{
+    if (employee1.Result > maxResult)
     {
-        if(item == letter)
-        {
-            count++;
-        }
+        employeeWithMaxResult = employee1;
+        maxResult = employee1.Result;
     }
-    Console.WriteLine(item + "=>" + count);
+    else if (employee2.Result > maxResult)
+    {
+        employeeWithMaxResult = employee2;
+        maxResult = employee2.Result;
+    }
+    else if (employee3.Result > maxResult)
+    {
+        employeeWithMaxResult = employee3;
+        maxResult = employee3.Result;
+    }
 }
+
+Console.WriteLine("Employee with best score" + maxResult + employeeWithMaxResult.Name + employeeWithMaxResult.Surname); 
+
+
 
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,31 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+
+        public Employee(string name, string surname, string age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+
+        public string Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }                
+        }
+        public void AddScore(int score)
+        {
+            this.score.Add(score);
+        }
+    }   
+}
