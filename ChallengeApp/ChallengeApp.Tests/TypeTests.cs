3146261c8d546aafe078c42d4c60b070618bf887
wojciechrajdom <wojciechrajdom@gmail.com>
2024-10-20 22:36:41 +0200

    Zadanie domowe 8

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,38 @@
+﻿
+namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+
+        [Test]
+        public void VarShouldReturnTheSameEqual()
+        {
+            // arrange
+            var number1 = 5;
+            var number2 = 5;
+            // act
+
+            // assert
+            Assert.AreEqual(number1, number2);
+
+        }
+
+        [Test]
+        public void UserShouldReturnTheSameParameters()
+        {
+            // arrange
+            var employee1 = GetUser("Wojtek");
+            var employee2 = GetUser("Wojtek");
+            // act
+
+            // assert
+            Assert.AreEqual(employee1.Name, employee2.Name);
+
+        }
+        private Employee GetUser(string name)
+        {
+            return new Employee(name);
+        }
+    }
+}
+
